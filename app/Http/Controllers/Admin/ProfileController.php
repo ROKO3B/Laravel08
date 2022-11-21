f3439d252c3dbd4c1a694f470bf0afd443c51494
ご自身の名前 <rokorokokoro3@gmail.com>
2022-11-21 06:48:05 +0000

    laravel16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,18 +17,82 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-    public function create()
+    // 以下を追記
+    public function create(Request $request)
     {
+        
+        // 以下を追記
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile =new Profile;
+        $form = $request->all();
+
+       
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        // admin/news/createにリダイレクトする
         return redirect('admin/profile/create');
     }
-    public function edit()
+    // 以下を追記
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
-    }
-    public function update()
-    {
-       return redirect('admin/profile/edit'); 
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
     }
     
+    public function edit(Request $request)
+    {
+        // News Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // News Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        
+       
+        
+        
+        unset($profile_form['_token']);
+        
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+
+        return redirect('admin/profile');
+    }
     
+    // 以下を追記
+
+    public function delete(Request $request)
+    {
+        // 該当するNews Modelを取得
+        $profile = Profile::find($request->id);
+
+        // 削除する
+        $profile->delete();
+
+        return redirect('admin/profile/');
+    }
 }
