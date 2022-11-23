7e7594c51ec9cb7005336dc10c5dd0f43907bfc9
ご自身の名前 <rokorokokoro3@gmail.com>
2022-11-23 05:13:42 +0000

    Laravel19error

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+use App\Http\Controllers\NewsController as PublicNewsController;
+Route::get('/', [PublicNewsController::class, 'index'])->name('news.index');
+
